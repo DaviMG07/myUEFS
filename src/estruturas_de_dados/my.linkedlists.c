61c8afcf1625ecf2df5802c8215c9839445db4b2
davi <ivad162@gmail.com>
2024-04-16 21:55:37 -0300

    just a commit

--- src/estruturas_de_dados/my.linkedlists.c
+++ src/estruturas_de_dados/my.linkedlists.c
@@ -9,23 +9,27 @@ typedef struct node{
 
 
 int printll(node);
-int appendll(node*, int);
+int append(node*, int);
+int push(node*, int);
 
-//TODO
-int insert(node, int);
-int push(node, int);
+/*TODO
+int clear(node*); 
+int insert(node*, int, int);  
 int pop(node, int);
-int clear(node);
 int search(node, int);
+*/
 
 int main(void){
   system("clear||cls");
 
   node head = NULL;
 
-  appendll(head, 10);
-  printf("%d", head);
+  append(&head, 10);
+  append(&head, 20);
+  push(&head, 8);
+  //insert(&head, 7, 1);
   printll(head);
+  printf("%d", clear(&head));
 
   printf("\n");
   return 0;
@@ -39,28 +43,60 @@ int printll(node head){
   node current = head;
   while (current != NULL){
     fflush(stdout);
-    printf("%d", current->value);
+    printf("%d ", current->value);
     current = current->next;
   }
   return 1;
 }
 
-int appendll(node* head, int value){
+int append(node* head, int value){
+  node current;
   node new = (node)malloc(sizeof(node));
   new->value = value;
   new->next = NULL;
+
   if (*head == NULL){
     *head = new;
-    printf("new\n");
-    return 1;
+    return 0;
   }
-  return 0;
-  /*
-  node* current = *head;
+  current = *head;
   while (current->next != NULL){
     current = current->next;
   }
+  new->prev = current;
   current->next = new;
   return 1;
-  */ 
 }
+
+int push(node* head, int value){
+  node new = (node)malloc(sizeof(node));
+  new->value = value;
+  new->next = NULL;
+  new->prev = NULL;
+
+  if (*head == NULL){
+    *head = new;
+    return 0;
+  }
+  node current = *head;
+
+  new->next = current;
+  current->prev = new;
+  *head = new;
+  return 1;
+}
+
+
+/*TODO
+int clear(node* head){
+  if (*head == NULL){
+    return 0;
+  }
+  node current = *head;
+  node next = current->next;
+  while (current){
+    current = next;
+  }
+  return 1;
+}
+*/
